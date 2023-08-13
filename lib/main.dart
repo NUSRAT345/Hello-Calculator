67a35c0775d7db221b91e4f2350c28fd4e801f9c
NUSRAT345 <81955383+NUSRAT345@users.noreply.github.com>
2023-08-13 23:06:10 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,140 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(CalculatorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
+    return MaterialApp(
+      home: CalculatorScreen(),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class MyHomePage extends StatelessWidget {
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = '';
+  double _firstNumber = 0;
+  double _secondNumber = 0;
+  String _operation = '';
+
+  void _handleButtonPressed(String value) {
+    setState(() {
+      if (value == '+' || value == '-' || value == '*' || value == '/') {
+        _operation = value;
+        _firstNumber = double.parse(_output);
+        _output = '';
+      } else if (value == '=') {
+        _secondNumber = double.parse(_output);
+        switch (_operation) {
+          case '+':
+            _output = (_firstNumber + _secondNumber).toString();
+            break;
+          case '-':
+            _output = (_firstNumber - _secondNumber).toString();
+            break;
+          case '*':
+            _output = (_firstNumber * _secondNumber).toString();
+            break;
+          case '/':
+            if (_secondNumber != 0) {
+              _output = (_firstNumber / _secondNumber).toString();
+            } else {
+              _output = 'Error';
+            }
+            break;
+        }
+        _operation = '';
+      } else if (value == 'C') {
+        _output = '';
+        _operation = '';
+        _firstNumber = 0;
+        _secondNumber = 0;
+      } else {
+        _output += value;
+      }
+    });
+  }
+
+  Widget _buildButton(String text) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(4.0),
+        child: ElevatedButton(
+          onPressed: () => _handleButtonPressed(text),
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24.0),
+            ),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator App'),
+      ),
       body: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(labelText: ""),
-            keyboardType: TextInputType.number,
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _output,
+                style: TextStyle(fontSize: 48),
+              ),
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('+'),
+          Divider(
+            height: 1,
+            color: Colors.black,
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('-'),
+          Row(
+            children: [
+              _buildButton('7'),
+              _buildButton('8'),
+              _buildButton('9'),
+              _buildButton('/'),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('x'),
+          Row(
+            children: [
+              _buildButton('4'),
+              _buildButton('5'),
+              _buildButton('6'),
+              _buildButton('*'),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('/'),
+          Row(
+            children: [
+              _buildButton('1'),
+              _buildButton('2'),
+              _buildButton('3'),
+              _buildButton('-'),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('='),
+          Row(
+            children: [
+              _buildButton('C'),
+              _buildButton('0'),
+              _buildButton('='),
+              _buildButton('+'),
+            ],
           ),
         ],
       ),
